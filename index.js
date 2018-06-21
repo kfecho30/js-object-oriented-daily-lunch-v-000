b910bfc18cd752a5c04a6c0243af4a0af06ddf34
Kelsey Fecho <kfecho30@gmail.com>
2018-06-21 15:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Neighborhood {
   }
   meals(){
     return this.deliveries().map(delivery => {
-      return delivery.meal();
+      return delivery.meal().filter((value, index, self) => self.indexOf(value) === index);
     })
   }
 }
