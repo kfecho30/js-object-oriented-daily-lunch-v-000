02dd281e3edcf0d9009f027e5855cf623d3f09b3
Kelsey Fecho <kfecho30@gmail.com>
2018-06-21 14:41:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+let neighborhoodId = 0;
+let customerId = 0;
+let mealId = 0;
+let deliveryId = 0;
+
 
 class Neighborhood {
 
@@ -14,5 +19,5 @@ class Meal {
 }
 
 class Delivery {
-  
+
 }
