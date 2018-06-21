724cf9acda4423ec39ecfb1b07b3b3a80918330f
Kelsey Fecho <kfecho30@gmail.com>
2018-06-21 14:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ class Meal {
     })
   }
   customers(){
-    return store.customers.filter(customer => {
-      return customer.mealId === this.id;
+    return this.deliveries().map(delivery => {
+      return delivery.customerId;
     })
   }
 }
